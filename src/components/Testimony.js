32b1f376e8e36479c954d55c983d4ea7f636fbae
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-01-28 16:40:06 -0500

    Modify 'Testimony.js' file.
    Add content to make the 'Testimony.js' file a functional component.
    It has a image of Emma.

---
+++ src/components/Testimony.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Testimony() {
+  return (
+    <div className='testimony-wrapper'>
+      <img 
+        className='testimony-image'
+        src={require('../images/Emma.png')}
+        alt='Emma image'
+      />
+    </div>
+  );
+}
+
+export default Testimony
