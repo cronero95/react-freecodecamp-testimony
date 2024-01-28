e9be7930f7a92ca337532fe7d4de1e0036ec494b
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-01-28 17:57:59 -0500

    Add text elements to 'Testimony.js'.
    Modify 'App.js' to import and use the 'Testimony' component.
    Modify 'Testimony.js' to add text elements in the component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimony from './components/Testimony';
 
 function App() {
   return (
     <div className="App">
-      
+      <Testimony/>
     </div>
   );
 }
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -6,8 +6,19 @@ function Testimony() {
       <img 
         className='testimony-image'
         src={require('../images/Emma.png')}
-        alt='Emma image'
+        alt='A young woman with black hair, white skin, and blue eyes'
       />
+      <div className='testimony-text-wrapper'>
+        <p className='tetimony-name'>
+          Emma Bostian from Sweden
+        </p>
+        <p className='testimony-job'>
+          Software Engineer at Spotify
+        </p>
+        <p className='testimony-text'>
+          I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.
+        </p>
+      </div>
     </div>
   );
 }
